1a6a7818a7edca3c3296cdb333b66e71bbe76747
panxin22911 <2291162503@qq.com>
2024-04-14 18:26:17 +0800

    简单的添加关于比较大小的宏的测试，更多的gtest用到时再去看

--- hello_test.cpp
+++ hello_test.cpp
@@ -3,10 +3,35 @@
 //
 #include <gtest/gtest.h>
 
+
+#define MIN(x, y)({ typeof(x)_x = (x);typeof(y)_y = (y);(void)(&_x==&_y);_x<_y?_x:_y;})
+#define MAX(x, y)({ typeof(x)_x = (x);typeof(y)_y = (y);(void)(&_x==&_y);_x>_y?_x:_y;})
+
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
 // Expect two strings not to be equal.
-EXPECT_STRNE("hello", "world");
+    EXPECT_STRNE("hello", "world");
+//    EXPECT_STRNE("hello", "hello");
+//    Expected: ("hello") != ("hello"), actual: "hello" vs "hello"
+
 // Expect equality.
-EXPECT_EQ(7 * 6, 42);
+    EXPECT_EQ(7 * 6, 42);
+
+}
+
+
+TEST(min_of_two_number, min) {
+    {
+        EXPECT_EQ(MIN(9, 4), 4);
+    }
+    {
+        EXPECT_EQ(MAX(9, 4), 9);
+    }
+
+}
+
+TEST(min_of_two_number, max) {
+//    EXPECT_EQ(MIN(9, 4), 4);
+    EXPECT_EQ(MAX(9, 4), 9);
+
 }
